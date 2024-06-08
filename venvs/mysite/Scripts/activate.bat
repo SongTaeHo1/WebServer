004fe1d79efa01d559239ba0b988c955e72617f7
SongTaeHo1 <ehgf7569@gmail.com>
2024-06-09 00:51:12 +0900

    [FEAT] 3-15 검색및정렬 구현

--- venvs/mysite/Scripts/activate.bat
+++ venvs/mysite/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\git\WebServerAD\venvs\mysite
+set VIRTUAL_ENV=C:\Users\USER\Desktop\git\WebServer\venvs\mysite
 
 if not defined PROMPT set PROMPT=$P$G
 
